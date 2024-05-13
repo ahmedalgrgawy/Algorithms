059afacad898fdc83778d1edfdf8aa1d81b6a2fa
Ahmed Algrgawy <ahmedalgrgawy77@gmail.com>
2024-05-14 02:53:10 +0300

    Ai Lab

---
+++ BFS/BFS.py
@@ -0,0 +1,28 @@
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+visited = [] # List for visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node): #function for BFS
+  visited.append(node)
+  queue.append(node)
+
+  while queue:          # Creating loop to visit each node
+    m = queue.pop(0) 
+    print (m, end = " ") 
+
+    for neighbor in graph[m]:
+      if neighbor not in visited:
+        visited.append(neighbor)
+        queue.append(neighbor)
+
+# Driver Code
+print("Following is the Breadth-First Search")
+bfs(visited, graph, '5')    # function calling
